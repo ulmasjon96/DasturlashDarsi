8b484776c6fc5d8294354aad52ea563b6508ad37
Ismatov O'lmasjon <almatov9642@mail.ru>
2025-04-30 16:01:59 +0500

    Yangilandi

--- 4. Aprel/10-dars. JS DOM Amaliyot/app.js
+++ 4. Aprel/10-dars. JS DOM Amaliyot/app.js
@@ -2,25 +2,25 @@
  * Counter app
  */
 
-const numberElement = document.querySelector(".number");
-const container = document.querySelector(".container");
-const buttonGroup = document.querySelector(".button-group");
+const numberElement = document.querySelector('.number');
+const container = document.querySelector('.container');
+const buttonGroup = document.querySelector('.button-group');
 let count = 0;
-buttonGroup.addEventListener("mousedown", changeNumber);
+buttonGroup.addEventListener('mousedown', changeNumber);
 
 function changeNumber(event) {
-  console.log(event);
-  if (event.target.id === "plus") count++;
-  if (event.target.id === "minus") count--;
-  if (count < 0) {
-    numberElement.style.color = "red";
-    container.style.borderColor = "red";
-  } else if (count > 0) {
-    numberElement.style.color = "green";
-    container.style.borderColor = "green";
-  } else {
-    numberElement.style.color = "inherit";
-    container.style.borderColor = "inherit";
-  }
-  numberElement.textContent = count;
+	console.log(event);
+	if (event.target.id === 'plus') count++;
+	if (event.target.id === 'minus') count--;
+	if (count < 0) {
+		numberElement.style.color = 'red';
+		container.style.borderColor = 'red';
+	} else if (count > 0) {
+		numberElement.style.color = 'green';
+		container.style.borderColor = 'green';
+	} else {
+		numberElement.style.color = 'inherit';
+		container.style.borderColor = 'inherit';
+	}
+	numberElement.textContent = count;
 }
