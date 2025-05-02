55ccd18d6401b48d91e7d1b458ee9eb311682330
Ismatov O'lmasjon <almatov9642@mail.ru>
2025-05-02 09:35:44 +0500

    Yangilandi

---
+++ 5. May/1-dars. js/js/app.js
@@ -0,0 +1,26 @@
+// // o'zgaruvchi yaratildi
+// let user = {
+// 	name: 'John',
+// 	age: 30,
+// 	isAdmin: true,
+
+//   // sayHi funksiyasi yaratildi va user ga qo'shildi
+//   sayHi() {
+//     // this - user yordamida o'zgaruvchini ichidagi elementlarni chaqiradi va console ga chiqaradi
+// 		console.log(this.isAdmin);
+// 	},
+// };
+
+// user.sayHi();
+
+// function User(name, age) {
+// 	this.name = name;
+// 	this.isAdmin = false;
+// 	this.age = age;
+// }
+
+// let user = new User('John', 30);
+// console.log(user.age);
+// console.log(user.name);
+// console.log(user.isAdmin);
+
