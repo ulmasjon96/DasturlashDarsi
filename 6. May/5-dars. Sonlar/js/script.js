58ddb98ea399f15a99113502f0c2d4df0890d8ee
premiumelectro91 <premiumelectro91@xmail.ru>
2025-06-02 11:52:32 +0500

    Yangilandi

---
+++ 6. May/5-dars. Sonlar/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let value;
+
+value = 13;
+value = -13;
+value = 13.13;
+value = -13.13;
+value = Infinity;
+value = -Infinity;
+
+value = 15 + 13;
+value = 15 - 13;
+value = 15 * 13;
+value = 15 / 13;
+value = 15 % 2; // sonni 2 ga bo'lganda qoldig'ini aniqlash
+value = 17 % 2;
+value = 15 ** 13; // darajaga ko'paytirish
+value = 2 ** 8;
+value = 64 ** (1 / 2); // 64 ni kvadrat ildizga qo'yish
+
+console.log(value);
