c0fe073112cfb3c94f5a1878730fa16d2544d25f
premiumelectro91 <premiumelectro91@xmail.ru>
2025-06-02 11:29:45 +0500

    Yangilandi

---
+++ 6. May/3-dars. Ma'lumot turlari/js/script.js
@@ -0,0 +1,44 @@
+/** Primitivlar
+ * Obyektlar
+ */
+
+/** Primitivlar
+ *
+ * 1. String
+ * 2. Number
+ * 3. Boolean
+ * 4. Null
+ * 5. Undefined
+ * 6. Symbol
+ * 7. BigInt
+ *
+ */
+
+/** Obyektlar
+ * 1. Object
+ * 2. Array
+ * 3. Function
+ * 4. Object date
+ */
+
+// string
+
+let str;
+
+str = 'Hello'; // 2 talik qo'shtirnoq
+str = 'Hello'; // 1 talik qo'shtirnoq
+str = `Hello`; // bektik qo'shtirnoq
+
+// console.log(str);
+// console.log(typeof str);
+
+// number
+
+let num;
+num = 12;
+num = -12;
+num = 3.456;
+num = -3.456;
+num = Infinity;
+num = -Infinity;
+num = NaN;
